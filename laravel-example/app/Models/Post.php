8f921b8f9dacb058e69a2031916cd428ff587753
Ronal David Valderrama Gomez <189985514+ronaldavidvalderrama@users.noreply.github.com>
2025-08-28 14:00:36 +0000

    feat: :construction: mapero y resstructuracion metodo post

--- laravel-example/app/Models/Post.php
+++ laravel-example/app/Models/Post.php
@@ -4,12 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
+use Illuminate\Database\Eloquent\SoftDelete;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDelete;
     protected $table = 'posts';
 
-    protected $fillable = ['title', 'content', 'status'];
+    protected $fillable = [
+        'title', 'content', 'status',
+        'published_at', 'conver_image', 'tags', 'meta'
+    ];
+
+    protected $casts = [
+        'published_at' => 'datatime',
+        'tags' => 'array',
+        'meta' => 'meta'
+    ];
+
+    public function categories() {
+        // TABLA PIVOTE post_category
+
+        return $this->belongsToMany(category::class)->using(CategoryPost::class)->withTimestamps();
+    }
 }
